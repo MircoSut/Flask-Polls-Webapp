cf68e1945828da82bed288f1e912e8be52e48b74
MircoSut <mircosut76@gmail.com>
2023-11-29 10:50:50 +0100

    Added Create page in the structure

--- website/views.py
+++ website/views.py
@@ -9,29 +9,7 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
-    if request.method == "POST":
-        question = request.form.get("question")
-        option_1 = request.form.get("option_1")
-        option_2 = request.form.get("option_2")
-        option_3 = request.form.get("option_3")
-        option_4 = request.form.get("option_4")
-
-        if len(question) < 1:
-            flash("Question is too short.", category="error")
-        elif not(option_1):
-            flash("Option 1 and Option 2 are required.", category="error")
-        elif not(option_2):
-            flash("Option 1 and Option 2 are required.", category="error")
-        else:
-            new_poll = Poll(question=question, 
-                            option_1 = option_1, 
-                            option_2 = option_2, 
-                            option_3 = option_3, 
-                            option_4 = option_4,
-                            user_id=current_user.id)
-            db.session.add(new_poll)
-            db.session.commit()
-            flash("Poll Created!", category="success")
+    
     return render_template("home.html", user=current_user)
 
 @views.route("/delete-poll", methods=["POST"])
@@ -59,3 +37,33 @@ def vote():
 
 
     return render_template("vote.html", user=current_user, polls=polls, user_names=user_names)
+
+@views.route("create", methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        question = request.form.get("question")
+        option_1 = request.form.get("option_1")
+        option_2 = request.form.get("option_2")
+        option_3 = request.form.get("option_3")
+        option_4 = request.form.get("option_4")
+
+        if len(question) < 1:
+            flash("Question is too short.", category="error")
+        elif not(option_1):
+            flash("Option 1 and Option 2 are required.", category="error")
+        elif not(option_2):
+            flash("Option 1 and Option 2 are required.", category="error")
+        else:
+            new_poll = Poll(question=question, 
+                            option_1 = option_1, 
+                            option_2 = option_2, 
+                            option_3 = option_3, 
+                            option_4 = option_4,
+                            user_id=current_user.id)
+            db.session.add(new_poll)
+            db.session.commit()
+            flash("Poll Created!", category="success")
+            
+    return render_template("create.html", user=current_user)
+        
